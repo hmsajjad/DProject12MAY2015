a801d6b52a042a11762ea65036f012dd15410c0e
hmsajjad <hmsajjad89@gmail.com>
2015-05-12 13:17:58 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sajjad on 12/05/2015.
+ */
+
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
+});
+
